3e790151f2fae361c559f723e5ddf031c4b82214
yuta-satou <68938732+yuta-satou@users.noreply.github.com>
2020-11-11 13:45:56 +0900

    rspecの導入

---
+++ spec/factories/items.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :item do
+    title {"abe"}
+    sub_title {"kkkgmail"} 
+    target_amount {100000} 
+    day_id { Faker::Date.between(from: '2014-09-23', to: '2014-09-25') }
+    remaining_days {20}
+end
---
+++ spec/factories/orders.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :order do
+    token {"sdfadfasjdkfk"}
+    price {"10000"}
+  end
+end
