e977913426d549d4a21d271574a456c3baaff81f
yuta-satou <68938732+yuta-satou@users.noreply.github.com>
2020-11-17 17:11:24 +0900

    TOP画像のプレビュー実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,8 @@ require("../card")
 require("../price")
 require("../tab_change")
 require("../achievement_bar")
+require("../preview")
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
