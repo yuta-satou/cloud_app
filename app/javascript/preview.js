e160b483d4409e9d6fefcf862daffd6e3ffccfa6
yuta-satou <68938732+yuta-satou@users.noreply.github.com>
2020-11-19 02:10:07 +0900

    Update preview.js

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,22 +1,23 @@
+if (document.URL.match( /new/ ) && document.URL.match( /edit/ )){
+  document.addEventListener('DOMContentLoaded', function(){
+    const ImageList = document.getElementById('image-list');
+    document.getElementById('item-image').addEventListener('change', function(e){
+      
+      const imageContent = document.querySelector('img');
+      if(imageContent){
+        imageContent.remove();
+      }
+      
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
 
-document.addEventListener('DOMContentLoaded', function(){
-  const ImageList = document.getElementById('image-list');
-  document.getElementById('item-image').addEventListener('change', function(e){
-    
-    const imageContent = document.querySelector('img');
-    if(imageContent){
-      imageContent.remove();
-    }
-    
-    const file = e.target.files[0];
-    const blob = window.URL.createObjectURL(file);
-
-    const imageElement = document.createElement('div');
-    const blobImage = document.createElement('img');
-    blobImage.setAttribute('src', blob);
-    blobImage.width = 350;
-    blobImage.height = 300;
-    imageElement.appendChild(blobImage);
-    ImageList.appendChild(imageElement);
+      const imageElement = document.createElement('div');
+      const blobImage = document.createElement('img');
+      blobImage.setAttribute('src', blob);
+      blobImage.width = 350;
+      blobImage.height = 300;
+      imageElement.appendChild(blobImage);
+      ImageList.appendChild(imageElement);
+    });
   });
-});
+}
